7900c2271270d16299da86c80e2ed6d95f0b4586
sax59 <waquetv@gmail.com>
2023-10-19 12:19:38 +0200

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
+        <p>Welcome to your P2 project</p>
 
         <Counter />
 
