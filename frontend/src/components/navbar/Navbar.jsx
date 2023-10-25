0f9a20ba20df292cbed1eb0250b4539001093837
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-25 17:10:47 +0200

    Update Navbar.jsx

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -5,11 +5,17 @@ function Navbar() {
     <div className="Navbar">
       <ul className="Navbar_buton">
         <li className="Navbar_li a">
-          <button type="submit">Retour</button>
+          <button className="Navbar_detourbuton" type="submit">
+            ◀︎ Retour
+          </button>
         </li>
-        <li className="Navbar_title">Catalogue</li>
+        <li className="Navbar_title">
+          <h1>Catalogue</h1>
+        </li>
         <li className="Navbar_li a">
-          <button type="submit">Page d'accueil</button>
+          <button className="Navbar_detourbuton" type="submit">
+            Page d'accueil ▶︎
+          </button>
         </li>
       </ul>
     </div>
