351775f5b8979d30900f1abb7f64ffefe6f45568
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-10-26 15:16:33 +0200

    add the main page component with a backgroundImage  and scss style in the src/frontend/pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
+/* import Counter from "./components/Counter";
+import logo from "./assets/logo.svg"; */
 import "./App.css";
+import Main from "./pages/main/main";
 
 function App() {
   const [arts, setArts] = useState();
@@ -13,36 +14,8 @@ function App() {
   }, []);
   console.info(arts);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to your P2 project</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Main />
     </div>
   );
 }
---
+++ frontend/src/pages/main/main.jsx
@@ -0,0 +1,13 @@
+import "./main.scss";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main__background">
+        <img src="src/assets/bg_main.jpg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
