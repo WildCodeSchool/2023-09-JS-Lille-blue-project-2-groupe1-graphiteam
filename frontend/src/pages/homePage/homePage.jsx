eb9bf6fd81203baffbff7ee9fd487571fb60b7c2
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-10-26 16:49:32 +0200

    change wording

--- frontend/src/pages/homePage/homePage.jsx
+++ frontend/src/pages/homePage/homePage.jsx
@@ -2,8 +2,8 @@ import "./homePage.scss";
 
 function HomePage() {
   return (
-    <div className="main">
-      <div className="main__background">
+    <div className="homepage">
+      <div className="homepage__background">
         <h1>homepage</h1>
         <img src="src/assets/bg_main.jpg" alt="" />
       </div>
