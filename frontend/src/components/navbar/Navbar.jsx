ef854fddd542ef5ccaac3c4082024785236a89ab
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-30 14:57:33 +0100

    react icon add

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,31 +1,26 @@
-import "./Navbar.scss";
+import "./navbar.scss";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <ul className="Navbar_buton">
+      <ul className="Navbar_thebuton">
         <li className="Navbar_li a">
-          <button
-            className="Navbar_detourbuton"
-            type="button"
-            onClick={() => {}}
-          >
-            ◀︎ Retour
+          <button className="Navbar_buton" type="button" onClick={() => {}}>
+            <AiFillCaretLeft size="35px" /> Retour
           </button>
         </li>
         <li className="Navbar_title">
           <h1>Catalogue</h1>
         </li>
         <li className="Navbar_li a">
-          <button
-            className="Navbar_detourbuton"
-            type="button"
-            onClick={() => {}}
-          >
-            Page d'accueil ▶︎
+          <button className="Navbar_buton" type="button" onClick={() => {}}>
+            Page d'accueil <AiFillCaretRight size="35px" />
           </button>
         </li>
       </ul>
+      <h3> </h3>
     </div>
   );
 }
