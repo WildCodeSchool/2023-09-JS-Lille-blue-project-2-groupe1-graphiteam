bf9c6da0b7bad0e9c5b7026da5bd27e713e909d7
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-08 17:26:26 +0100

    Update ArtCard.jsx

--- frontend/src/components/ArtCard/ArtCard.jsx
+++ frontend/src/components/ArtCard/ArtCard.jsx
@@ -3,7 +3,7 @@ import "./ArtCard.scss";
 function ArtCard() {
   return (
     <div className="card">
-      <img src="" alt="" />
+      <img className="card__picture" src="" alt="" />
       <article>
         <h4>artist</h4>
         <p>description</p>
