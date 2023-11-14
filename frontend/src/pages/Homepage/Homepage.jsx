70b5cdad654e73d6f652de7b975c5581e0b8b7a0
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-11-14 20:22:58 +0100

    add V2 dropdowm menu style

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -7,13 +7,8 @@ function Homepage() {
     <div className="homepage">
       <PresentationBox />
       <div className="homepage--button--container">
-        <h4 className="drip">test test test</h4>
         <h1 className="homepage--title fancy"> Lille Street Art</h1>
-        {/*  <img
-          className="homepage--banner"
-          src="src/assets/dirtyBrick.png"
-          alt=""
-        /> */}
+
         <DropdownMenu />
       </div>
     </div>
