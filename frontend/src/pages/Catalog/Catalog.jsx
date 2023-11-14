ac7d9cca68aaf6c11d02e8cd80f58539147b05a4
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-14 17:00:13 +0100

    Update Catalog.jsx

--- frontend/src/pages/Catalog/Catalog.jsx
+++ frontend/src/pages/Catalog/Catalog.jsx
@@ -14,7 +14,20 @@ function Catalog() {
   return (
     <div className="catalog" style={{ overflow: "scroll", height: "100vh" }}>
       {arts
-        ? arts.map((art) => <ArtCard key={art.id} img={art.imgSrc} />)
+        ? arts.map((art) => (
+            <ArtCard
+              key={art.id}
+              img={art.imgSrc}
+              artist={art.artist}
+              city={art.city}
+              description={art.description}
+              district={art.district}
+              street={art.street}
+              type={art.type}
+              year={art.year}
+              lieu={art.lieu}
+            />
+          ))
         : "Loading"}
     </div>
   );
